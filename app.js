e764541621cf31eaabeb0dc28b1d0fa5c4bdf9ca
Steppenw <s.pagani73@gmail.com>
2021-05-12 21:34:24 +0200

    Terminato, no bonus.

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const app = new Vue({
   el: "#app",
   data: {
-    albumsList: []
+    albumsList: [],
+    loading: false
   },
   methods: {
     
   },
   mounted() {
-    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((resp) => {
+    
+    const ajaxAlbumsList = [];
+    
+    this.loading = true;
+    
+    /*axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((resp) => {
       //console.log('ajax', resp.data.response);
-      this.albumsList = resp.data.response;
+      if (resp.data.response.length) {
+        this.albumsList = resp.data.response;
+
+        this.loading = false;
+      }
       //console.log('albumsList', this.albumsList);
-    });
+    });*/
+    
+    for (let i=0; i<10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((resp) => {
+        //console.log(i + 1, resp.data.response[i]);
+        ajaxAlbumsList.push(resp.data.response[i]);
+
+        if (ajaxAlbumsList.length === 10) {
+          this.albumsList = ajaxAlbumsList;
+
+          this.loading = false;
+        }
+      });
+    }
   }
 });
